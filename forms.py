ef9346a1a78dfd451c3829b36abaecd321fbebb8
Madhan098 <affiliatemadhan@gmail.com>
2025-11-07 23:38:41 +0530

    FIX: Added ALL missing form fields to InvitationForm
    PROBLEM:
    - Form was missing host_name and many other fields
    - Template was trying to use {{ form.host_name }} but field didn't exist
    - Error: 'InvitationForm object has no attribute host_name'
    
    SOLUTION:
    - Added ALL fields that create.html template uses:
      * host_name, venue, message
      * bride_name, groom_name
      * main_image, bride_image, groom_image (FileField)
      * rsvp_contact, rsvp_email
      * language, font_style (SelectField)
    
    RESULT:
    - Form now has all required fields
    - Template should render without errors

--- forms.py
+++ forms.py
@@ -1,13 +1,33 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, DateField, TimeField, FileField, SelectField
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, TextAreaField, DateField, TimeField, SelectField
 from wtforms.validators import DataRequired, Optional
 
 class InvitationForm(FlaskForm):
     """Form for creating invitations"""
+    # Basic fields
     title = StringField('Title', validators=[DataRequired()])
     event_date = DateField('Event Date', validators=[DataRequired()])
     event_time = TimeField('Event Time', validators=[Optional()])
-    description = TextAreaField('Description', validators=[Optional()])
     venue = StringField('Venue', validators=[Optional()])
+    host_name = StringField('Host Name', validators=[Optional()])
     message = TextAreaField('Message', validators=[Optional()])
+    description = TextAreaField('Description', validators=[Optional()])
+    
+    # Wedding-specific fields
+    bride_name = StringField('Bride Name', validators=[Optional()])
+    groom_name = StringField('Groom Name', validators=[Optional()])
+    
+    # Image upload fields
+    main_image = FileField('Main Image', validators=[Optional(), FileAllowed(['jpg', 'jpeg', 'png', 'gif'], 'Images only!')])
+    bride_image = FileField('Bride Image', validators=[Optional(), FileAllowed(['jpg', 'jpeg', 'png', 'gif'], 'Images only!')])
+    groom_image = FileField('Groom Image', validators=[Optional(), FileAllowed(['jpg', 'jpeg', 'png', 'gif'], 'Images only!')])
+    
+    # RSVP fields
+    rsvp_contact = StringField('RSVP Contact', validators=[Optional()])
+    rsvp_email = StringField('RSVP Email', validators=[Optional()])
+    
+    # Customization fields
+    language = SelectField('Language', choices=[('en', 'English'), ('es', 'Spanish'), ('fr', 'French'), ('de', 'German'), ('it', 'Italian'), ('pt', 'Portuguese')], validators=[Optional()])
+    font_style = SelectField('Font Style', choices=[('elegant', 'Elegant'), ('modern', 'Modern'), ('classic', 'Classic'), ('playful', 'Playful')], validators=[Optional()])
 
